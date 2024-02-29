7de605d1ec6ada6332c3ca9e4cf165b441b39113
ArielFdz <arieljr11@outlook.com>
2024-02-29 03:31:44 -0600

    delete comments

--- deploy.sh
+++ deploy.sh
@@ -1,28 +1,23 @@
 #!/bin/bash
 
-# Función para detener NGINX
 stop_nginx() {
     systemctl stop nginx
 }
 
-# Función para iniciar NGINX
 start_nginx() {
     systemctl start nginx
 }
 
-# Función para iniciar NGROK en segundo plano
 start_ngrok() {
     ngrok http http://localhost:80 > /dev/null &
     echo "NGROK iniciado."
 }
 
-# Función para obtener la URL de NGROK
 get_ngrok_url() {
     ngrok_url=$(curl -s localhost:4040/api/tunnels | jq -r '.tunnels[0].public_url')
     echo "La URL de NGROK es: $ngrok_url"
 }
 
-# Función principal
 main() {
     stop_nginx
     killall ngrok
@@ -37,5 +32,4 @@ main() {
     sleep infinity
 }
 
-# Llamar a la función principal
 main
